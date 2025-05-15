0762d09d7fe5ba98aa8140d2d35fa8828f33002f
Khai <khaipham2204@gmail.com>
2025-05-15 22:07:00 +0700

    commit

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class Todo(models.Model):
+
+class Todo(models.Model):  # E302 fixed: Added blank line
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
@@ -10,17 +11,20 @@ class Todo(models.Model):
     def __str__(self):
         return self.title
 
-class Category(models.Model):
+
+class Category(models.Model):  # E302 fixed: Added blank line
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
-class Comment(models.Model):
+
+class Comment(models.Model):  # E302 fixed: Added blank line
     todo = models.ForeignKey(Todo, on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Comment on {self.todo.title} by {self.id}"
+# W292 fixed: Added a newline at the end of the file
--- todo/tests.py
+++ todo/tests.py
@@ -1,83 +1,97 @@
 import pytest
-from django.utils import timezone
-from .models import Todo, Category, Comment
+
+
+from .models import Todo, Category, Comment  # E302 fixed: Added blank line
+
 
 @pytest.fixture
-def todo():
+def todo():  # E302 fixed: Added blank line
     return Todo.objects.create(
         title="Test Todo",
         description="Test Description",
         completed=False
     )
 
+
 @pytest.fixture
-def category():
+def category():  # E302 fixed: Added blank line
     return Category.objects.create(
         name="Test Category",
         description="Test Category Description"
     )
 
+
 @pytest.fixture
-def comment(todo):
+def comment(todo):  # E302 fixed: Added blank line
     return Comment.objects.create(
         todo=todo,
         text="Test Comment"
     )
 
+
 # Todo Tests
 @pytest.mark.django_db
-def test_todo_creation(todo):
+def test_todo_creation(todo):  # E302 fixed: Added blank line
     assert todo.title == "Test Todo"
     assert todo.description == "Test Description"
-    assert todo.completed == False
+    assert todo.completed is False  # E712 fixed: Changed comparison to `is False`
     assert todo.created_at is not None
     assert todo.updated_at is not None
 
-@pytest.mark.django_db
-def test_todo_str_method(todo):
-    assert str(todo) == "Test Todo"
 
 @pytest.mark.django_db
-def test_todo_update(todo):
+def test_todo_str_method(todo):  # E302 fixed: Added blank line
+    assert str(todo) == "Test Todo"
+
+
+@pytest.mark.django_db
+def test_todo_update(todo):  # E302 fixed: Added blank line
     old_updated_at = todo.updated_at
     todo.title = "Updated Todo"
     todo.save()
     assert todo.title == "Updated Todo"
     assert todo.updated_at != old_updated_at
 
+
 # Category Tests
 @pytest.mark.django_db
-def test_category_creation(category):
+def test_category_creation(category):  # E302 fixed: Added blank line
     assert category.name == "Test Category"
     assert category.description == "Test Category Description"
 
-@pytest.mark.django_db
-def test_category_str_method(category):
-    assert str(category) == "Test Category"
 
 @pytest.mark.django_db
-def test_category_unique_name(category):
+def test_category_str_method(category):  # E302 fixed: Added blank line
+    assert str(category) == "Test Category"
+
+
+@pytest.mark.django_db
+def test_category_unique_name(category):  # E302 fixed: Added blank line
     with pytest.raises(Exception):
         Category.objects.create(name="Test Category")
 
+
 # Comment Tests
 @pytest.mark.django_db
-def test_comment_creation(comment, todo):
+def test_comment_creation(comment, todo):  # E302 fixed: Added blank line
     assert comment.todo == todo
     assert comment.text == "Test Comment"
     assert comment.created_at is not None
 
-@pytest.mark.django_db
-def test_comment_str_method(comment, todo):
-    assert str(comment) == f"Comment on {todo.title} by {comment.id}"
 
 @pytest.mark.django_db
-def test_comment_related_name(comment, todo):
+def test_comment_str_method(comment, todo):  # E302 fixed: Added blank line
+    assert str(comment) == f"Comment on {todo.title} by {comment.id}"
+
+
+@pytest.mark.django_db
+def test_comment_related_name(comment, todo):  # E302 fixed: Added blank line
     assert todo.comments.count() == 1
     assert todo.comments.first() == comment
 
+
 @pytest.mark.django_db
-def test_comment_cascade_delete(comment, todo):
+def test_comment_cascade_delete(comment, todo):  # E302 fixed: Added blank line
     comment_id = comment.id
     todo.delete()
     assert Comment.objects.filter(id=comment_id).count() == 0
